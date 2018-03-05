1aa53c276d59c83234804f839a6122c9813b81f6
TrangLee28 <lelinhtrang28@gmail.com>
2018-03-05 20:59:36 +0700

    connection

---
+++ QLNS/QLNS/class/globalParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuctapNhom
+{
+    class globalParameter
+    {
+        public static String str = @"Data Source=DESKTOP-66N7HT9;Initial Catalog=QuanLyNhanSu;Integrated Security=True";
+    }
+}
